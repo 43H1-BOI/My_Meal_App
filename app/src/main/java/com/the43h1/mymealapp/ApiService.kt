f90fdab726340fc994616a4749a8290002c0da4f
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-02 20:07:13 +0530

    Added Internet Call

---
+++ app/src/main/java/com/the43h1/mymealapp/ApiService.kt
@@ -0,0 +1,19 @@
+package com.the43h1.mymealapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+// object to fetch response from web using retrofit
+private val recipe = Retrofit.Builder()
+    .baseUrl("www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+// object to store response
+val recipeServices: ApiService = recipe.create(ApiService::class.java)
+
+interface ApiService {
+    @GET("categories.php")
+    suspend fun getCategories(): CategoriesResponse
+}
